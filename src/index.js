32a81abab75d6a401c582e1afab29592e8599562
Agie Fadillah <arigierf@gmail.com>
2024-01-14 17:57:32 +0700

    Show Data in Tables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import cars from './practice';
+
+// import animals, { useAnimals } from "./data";
+
+//Destructuring Arrays
+// console.log(animals);
+// const [cat, dog] = animals;
+// console.log(cat);
+
+// const [animal, makeSound] = useAnimals(cat);
+// console.log(animal);
+// makeSound();
+
+//Destructuring Objects
+// const { name, sound} = cat;
+// const { name: catName, sound: catSound } = cat;
+// const { name = "Fluffy", sound = "Purr" } = cat;
+// const {feedingRequirements: {food, water} } = cat;
+// console.log(food);
+
+const [honda, tesla] = cars;
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+} = tesla;
+
+const {
+  coloursByPopularity: [hondaTopColour],
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour],
+} = tesla;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -20,6 +56,5 @@ root.render(
         <td>{hondaTopColour}</td>
       </tr>
     </table>
-    ,
   </React.StrictMode>
 );
